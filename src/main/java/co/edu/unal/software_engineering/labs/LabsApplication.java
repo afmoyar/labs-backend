4f5c43741c4933eb0d47fb98fce33e3501e8c005
afmoyar <afmoyar@unal.edu.co>
2020-06-04 17:39:30 -0500

    this is a message

--- src/main/java/co/edu/unal/software_engineering/labs/LabsApplication.java
+++ src/main/java/co/edu/unal/software_engineering/labs/LabsApplication.java
@@ -3,6 +3,7 @@ package co.edu.unal.software_engineering.labs;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//this is a comment
 @SpringBootApplication
 public class LabsApplication{
 
